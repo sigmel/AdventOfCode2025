9eff6f5e0ff84ef388e37a09edff9376a1414ac1
sigmel <sigmel@gmail.com>
2025-12-07 20:40:44 -0600

    Day 1 - Part 2

--- Project1/Project1.py
+++ Project1/Project1.py
@@ -9,12 +9,22 @@ for line in text:
     count = int(line[1:])
 
     move = 1 if dir == 'R' else -1
+    adjust = move * count
 
-    dial += move * count
+    # count how many full rotations we do
+    password += abs(int(adjust / 100))
+    adjust = int(abs(adjust) % 100) * move
 
-    dial = dial % 100
+    old_dial = dial
+    dial += adjust
 
-    if  dial == 0:
+    # I wonder if you could do this without renormalizing to 0-99 range, but I bet
+    # there are edge cases around 0 anyway, so just sticking with this
+    if dial >= 100 or dial == 0 or \
+        old_dial < 0 and dial > 0 or \
+        dial < 0 and old_dial > 0:
         password += 1
+    
+    dial = dial % 100
 
 print(password)
