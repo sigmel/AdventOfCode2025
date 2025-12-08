1402832e0f7dd26f70198539e197afb9a6ebea67
sigmel <sigmel@gmail.com>
2025-12-07 22:18:22 -0600

    Day 3 - Part 2

--- Project3/Project3.py
+++ Project3/Project3.py
@@ -3,15 +3,25 @@ with open('Project3/puzzle.txt', 'r') as f:
 
 max_joltages = []
 for line in text:
-    max = 0
+    max = []
 
-    for i,first in enumerate(line[:-1]):
-        for second in line[i+1:]:
-            combine = int(first + second)
-            if combine > max:
-                max = combine
+    num_digits = 12
+    line_length = len(line) + 1
 
-    max_joltages.append(max)
+    start = 0
+    for digit_num in range(num_digits):
+        max.append(line[start])
+
+        new_start = start
+        #print(line[start+1:line_length-num_digits+digit_num])
+        for digit_index, digit in enumerate(line[start+1:line_length-num_digits+digit_num]):
+            if int(digit) > int(max[-1]):
+                max[-1] = digit
+                new_start = start + digit_index + 1
+        
+        start = new_start + 1
+
+    max_joltages.append(int(''.join(max)))
 
 sum = 0
 for joltage in max_joltages:
