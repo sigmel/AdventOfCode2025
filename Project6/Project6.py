a0a4c90759847cd55e2f2d93738d554778cc3445
sigmel <sigmel@gmail.com>
2025-12-14 13:49:22 -0600

    Day 6 - Part 2

--- Project6/Project6.py
+++ Project6/Project6.py
@@ -3,15 +3,22 @@ with open('Project6/puzzle.txt', 'r') as f:
 
 operations = text.pop().split()
 
-numbers = []
-for line in text:
-    numbers.append([int(value) for value in line.split()])
-
-# transpose our array to make it easier to parse
-numbers = list(zip(*numbers))
+numbers = [[]]
+num_rows = len(text)
+num_columns = len(text[0])
+for i in range(num_columns - 1, -1, -1):
+    num = ''
+    for c in range(0, num_rows):
+        num += text[c][i]
+    
+    num = num.strip()
+    if len(num) > 0:
+        numbers[-1].append(int(num))
+    else:
+        numbers.append([])
 
 total = 0
-for op_index, op in enumerate(operations):
+for op_index, op in enumerate(reversed(operations)):
     if op == '*':
         sum = 1
         for num in numbers[op_index]:
