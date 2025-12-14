3e4b5059cbae63fea3ab3281bbd8008a9791a1e6
sigmel <sigmel@gmail.com>
2025-12-14 13:16:50 -0600

    Day 5 - Part 2

--- Project5/Project5.py
+++ Project5/Project5.py
@@ -1,22 +1,39 @@
 with open('Project5/puzzle.txt', 'r') as f:
     text = f.read().splitlines()
 
-ranges = []
-check_range = False
-
-fresh = 0
+fresh = []
 for line in text:
     if len(line) == 0:
-        check_range = True
-        continue # we found our blank line separator, so switch to evaluation
+        break
 
-    if not check_range:
-        ranges.append([int(num) for num in line.split('-')])
-    else:
-        value = int(line)
-        for range in ranges:
-            if value >= range[0] and value <= range[1]:
-                fresh += 1
-                break # we're fresh, no need to check more
+    range_values = list(int(num) for num in line.split('-'))
 
-print(fresh)
+    # find the unique range value
+    for fresh_range in fresh:
+        if range_values[0] >= fresh_range[0] and range_values[0] < fresh_range[1]:
+            range_values[0] = fresh_range[1]
+        if range_values[1] <= fresh_range[1] and range_values[1] > fresh_range[0]:
+            range_values[1] = fresh_range[0]
+            
+    if range_values[0] <= range_values[1]:
+        fresh.append(range_values)
+
+    # sort our ranges
+    fresh.sort()
+
+    # see if we can combine any
+    for fresh_index in range(len(fresh) - 2, -1, -1):
+        if fresh[fresh_index][1] == fresh[fresh_index+1][0]:
+            fresh[fresh_index][1] = fresh[fresh_index+1][1]
+            fresh.pop(fresh_index+1)
+
+        # see if this encompasses any of the prior ranges
+        for compare_index in range(len(fresh) - 1, fresh_index, -1):
+            if fresh[fresh_index][0] <= fresh[compare_index][0] and \
+                 fresh[fresh_index][1] >= fresh[compare_index][1]:
+                fresh.pop(compare_index)
+
+total = 0
+for fresh_range in fresh:
+    total += fresh_range[1]+1 - fresh_range[0]
+print(total)
